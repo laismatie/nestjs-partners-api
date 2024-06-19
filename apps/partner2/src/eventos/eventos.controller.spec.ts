f82533ee0516e84dfa1bdeea28528205ca824101
Laís <lmatiehara@gmail.com>
2024-06-18 20:17:07 -0400

    [UPDATE] using nest library and refactor structure

---
+++ apps/partner2/src/eventos/eventos.controller.spec.ts
@@ -0,0 +1,20 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { EventsController } from './eventos.controller';
+import { EventsService } from './eventos.service';
+
+describe('EventsController', () => {
+  let controller: EventsController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [EventsController],
+      providers: [EventsService],
+    }).compile();
+
+    controller = module.get<EventsController>(EventsController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
